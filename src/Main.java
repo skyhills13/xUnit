0d00610a0d3e11f1f31d5f5acf31155fafd0a7ce
soeunpark <soeunpark@soeunparkui-MacBook-Pro.local>
2016-10-08 02:40:55 +0900

    테스트메서드를 직접 호출하지 않고 인터페이스를 통해 동적으로 콜

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        WasRun testCase = new WasRun();
+    public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        WasRun testCase = new WasRun("testMethod");
         System.out.println(testCase.wasRun); //expect false
-        testCase.testMethod();
+        testCase.run();
+//        testCase.testMethod();
         System.out.println(testCase.wasRun); //expect true
     }
 }
--- src/WasRun.java
+++ src/WasRun.java
@@ -1,9 +1,29 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by soeunpark on 2016. 10. 8..
  */
-public class WasRun {
+public class WasRun{
     public boolean wasRun = false;
+    public String methodName;
 
+    public WasRun(String methodName){
+        this.methodName = methodName;
+    }
+
+    public void run() {
+        Class c = this.getClass();
+        try {
+        Method method = c.getMethod(this.methodName);
+        method.invoke(this);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    //private으로 해두고 삽질 대박함. reflection으로 메서드 가져오려면 public이어야함
+    //https://docs.oracle.com/javase/tutorial/reflect/member/methodInvocation.html
     public void testMethod(){
         wasRun = true;
     }
