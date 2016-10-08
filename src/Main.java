f98d4ddf270b8f04b3d7809351f5964c5f594bb2
soeunpark <skyhills13@gmail.com>
2016-10-08 09:06:24 +0900

    testSetUp테스트 추가

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
 
     public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-        TestCaseTest testCaseTest = new TestCaseTest("testRunning");
-        testCaseTest.run();
+        TestCaseTest testCaseTest1 = new TestCaseTest("testRunning");
+        testCaseTest1.run();
+
+        TestCaseTest testCaseTest2 = new TestCaseTest("testSetUp");
+        testCaseTest2.run();
     }
 }
