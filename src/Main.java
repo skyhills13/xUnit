94440a73e0e7dd2c279cb0971a7c83ac23f2ea1f
soeunpark <skyhills13@gmail.com>
2016-10-08 11:33:43 +0900

    setUp()을 의미에 맞게 활용할 수 있도록 리팩토링 - 로직 설명 추가

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
 
     public static void main(String[] args) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        /**
+         * 실행 로직
+         * 1. TestCaseTest 인스턴스 생성
+         *      => 파라미터로 넣은 메서드 이름은 super class인 TestCase에서 지정한 methodName 변수로 저장
+         * 2. run()을 실행하면 먼저 setUp()을 실행
+         *      => setUp()은 override했으므로 TestCaseTest의 setUp()이 호출
+         *      => 즉, WasRun인스턴스를 만들어 TestCaseTest의 변수로 저장
+         * 3. 리플렉션을 이용해 methodName에 해당하는 메서드 실행
+         *      => TestCaseTest의 testRunning()을 실행
+         *          a. WasRun 인스턴스의 run()이 실행
+         *              => 그 안에서 WasRun 인스턴스의 setUp()이 먼저 실행
+         *              => wasSetUp = true; wasRun = false;
+         *              => WasRun 인스턴스는 "testMethod"을 인자로 넣었으므로 testMethod의 내용인 wasRun = true이 됨
+         *          b. wasRun 값이 true인지 확인
+         */
         TestCaseTest testCaseTest1 = new TestCaseTest("testRunning");
         testCaseTest1.run();
 
--- src/TestCaseTest.java
+++ src/TestCaseTest.java
@@ -2,22 +2,24 @@
  * Created by soeunpark on 2016. 10. 8..
  */
 public class TestCaseTest extends TestCase {
+    private WasRun test;
 
     public TestCaseTest(String methodName){
         super(methodName);
     }
 
     public void testRunning(){
-        WasRun test = new WasRun("testMethod");
-        //test.run()에서 무조건 setUp을 호출해서 wasRun을 false로 넣었기 때문에 필요 없어짐
-        //assert false : test.wasRun;
         test.run();
         assert true : test.wasRun;
     }
 
     public void testSetUp(){
-        WasRun test = new WasRun("testMethod");
         test.run();
         assert test.wasSetUp;
+    }
+
+    @Override
+    public void setUp(){
+        test = new WasRun("testMethod");
     }
 }
